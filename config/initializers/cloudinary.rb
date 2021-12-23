968aca09deb26a892ae87dbf7562947d65ea1717
Leonardo Almeida <leoalmeidasa@gmail.com>
2021-12-23 09:27:57 -0300

    feat: bootstrap add button in frontend

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dbfiqm5nz'
+  config.api_key = '362568612274681'
+  config.api_secret = '4blPUnnAV06hKE0dzWUMKp8r6fQ'
+  config.secure = true
+  config.cdn_subdomain = true
+end
