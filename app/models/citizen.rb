fa28f61b5db49ed6e52c01cd794e87206e144b59
Leonardo <leoalmeidasa@gmail.com>
2022-03-14 15:30:40 -0300

    refactor(test model): add test citizen model

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -2,21 +2,26 @@
 
 class Citizen < ApplicationRecord
   has_one :address
+  has_one_attached :photo
   accepts_nested_attributes_for :address
 
   validates :name, :birth_date, :telephone, presence: true
   validates :cpf, presence: true, length: { minimum: 11, maximum: 11 }, uniqueness: true
   validates :cns, presence: true, length: { minimum: 15, maximum: 15 }, uniqueness: true
-  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   validates :telephone, length: { is: 11 }
-
-  has_one_attached :photo
 
   validate :validate_birth_date
 
+  validate :validate_cpf
+
   def validate_birth_date
     if birth_date.present? && birth_date > 18.years.ago
-        errors.add(:birth_date, 'inválida, favor preencha com uma data válida !')
+      errors.add(:birth_date, 'inválida, favor preencha com uma data válida !')
     end
+  end
+
+  def validate_cpf
+    errors.add(:cpf, 'inválido, favor preencha com um cpf válido !') unless CPF.valid?(cpf)
   end
 end
